07a0b9aa93a40558d592624e13acd1a546dd2ff7
Fabiana Anjos Campelo <fabyanjos@gmail.com>
2013-10-13 12:22:44 +0200

    update search

--- js/main.js
+++ js/main.js
@@ -11,9 +11,8 @@ function removeLine() {
 }
 
 function choose() {
-	var i = Math.floor(Math.random() * $("input[name=option]").length);
-	var text = $("input[name=option]")[i].value;
-	SC.get('/tracks', {q: $("input[name=option]")[i].value, limit: 50}, function(tracks) {
+	var text = $("input[name=option]")[0].value;
+	SC.get('/tracks', {q: text, limit: 50}, function(tracks) {
 	  var j = Math.floor(Math.random() * tracks.length);
 	  console.log(tracks[j])
 		SC.oEmbed(tracks[j].permalink_url, { auto_play: true }, function(oEmbed) {
@@ -21,7 +20,7 @@ function choose() {
 		  $("#chosen").text(text);
 		});
 	});
-	reset();
+
 }
 
 function play(type) {
@@ -30,13 +29,6 @@ function play(type) {
 	  console.log(tracks[j])
 		SC.oEmbed(tracks[j].permalink_url, { auto_play: true }, function(oEmbed) {
 		  $("#player").html(oEmbed.html);
-		  $("#chosen").text(text);
 		});
 	});
-}
-
-function reset() {
-	for(var i = 0; i < $("input[name=option]").length; i++) {
-		$("input[name=option]")[i].value = ""
-	}
 }
